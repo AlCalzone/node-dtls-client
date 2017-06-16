4e68adb080afa7e35834591dcfa02981a049670e
dominic.griesel <d.griesel@gmx.net>
2017-06-16 12:37:11 +0200

    added some CipherSuite definitions

--- src/TLS/CipherSuite.ts
+++ src/TLS/CipherSuite.ts
@@ -1,13 +1,15 @@
 ﻿import * as TLSTypes from "./TLSTypes";
 import { TLSStruct } from "./TLSStruct";
 
+export type CipherSuiteID = [number, number];
+
 export class CipherSuite extends TLSStruct {
 
 	static readonly __spec = {
 		value: new TLSTypes.Vector("uint8", 2)
 	}
 
-	constructor(public value = [0,0]) {
+	constructor(public value: CipherSuiteID = [0,0]) {
 		super(CipherSuite.__spec);
 	}
 
