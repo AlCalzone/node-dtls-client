66964f9e967eaf90ab6544adeef8c25a2bbd291e
dominic.griesel <d.griesel@gmx.net>
2017-06-19 10:38:16 +0200

    deleted unneccesary files

--- src/TLS/CompressionMethod.ts
+++
@@ -1,9 +0,0 @@
-﻿import * as TLSTypes from "./TLSTypes";
-import { TLSStruct } from "./TLSStruct";
-
-export enum CompressionMethod {
-	none = 0
-}
-export namespace CompressionMethod {
-	export const __spec = new TLSTypes.Enum("uint8", CompressionMethod);
-}
