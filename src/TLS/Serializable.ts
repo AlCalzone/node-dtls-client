30ed1b93d56c2b962c4bd21eb7870058878ea9dd
dominic.griesel <d.griesel@gmx.net>
2017-06-23 16:02:14 +0200

    trying mixin pattern for ISerializable

---
+++ src/TLS/Serializable.ts
@@ -0,0 +1,21 @@
+﻿// maybe this can be done with this syntax:
+// https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-2.html
+
+type Constructor<T> = new(...args: any[]) => T;
+
+function Serializable<T extends Constructor<{}>>(Base: T) {
+    return abstract class extends Base {
+        _tag: string;
+        constructor(...args: any[]) {
+            super(...args);
+            this._tag = "";
+        }
+    }
+}
+
+export abstract class Serializable<T> {
+
+	serialize(): Buffer {};
+	static deserialize(buf: Buffer, offset: number): T {};
+
+}
