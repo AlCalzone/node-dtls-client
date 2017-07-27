0dc9e9d2136e87abc1d57cd8232f64f507dc75b2
dominic.griesel <d.griesel@gmx.net>
2017-07-27 10:38:24 +0200

    improved error and timeout handling in the socket wrapper

--- src/dtls.ts
+++ src/dtls.ts
@@ -20,7 +20,7 @@ export module dtls {
 
 		// bind "message" event after the handshake is finished
 		if (callback != null) {
-			ret.on("connected", () => {
+			ret.once("connected", () => {
 				ret.on("message", callback);
 			});
 		}
@@ -45,11 +45,21 @@ export module dtls {
 				.on("close", this.udp_onClose.bind(this))
 				.on("error", this.udp_onError.bind(this))
 				;
+
+			// setup a timeout watcher. Default: 1000ms timeout, minimum: 100ms
+			this.options.timeout = Math.max(100, this.options.timeout || 1000);
+			this._udpConnected = false;
+			this._connectionTimeout = setTimeout(this.expectConnection, this.options.timeout);
+
+			// start the connection
 			this.udp.bind();
 		}
 
 		private recordLayer: RecordLayer;
 		private handshakeHandler: ClientHandshakeHandler;
+		private _handshakeFinished: boolean = false;
+		private _udpConnected: boolean;
+		private _connectionTimeout: NodeJS.Timer;
 
 		/**
 		 * Send the given data. It is automatically compressed and encrypted.
@@ -57,7 +67,10 @@ export module dtls {
 		send(data: Buffer, callback?: SendCallback) {
 
 			if (this._isClosed) {
-				throw new Error("the socket is closed. cannot send data.");
+				throw new Error("The socket is closed. Cannot send data.");
+			}
+			if (!this._handshakeFinished) {
+				throw new Error("DTLS handshake is not finished yet. Cannot send data.");
 			}
 
 			// send finished data over UDP
@@ -70,7 +83,7 @@ export module dtls {
 		}
 
 		close(callback?: CloseEventHandler) {
-			if (callback) this.on("close", callback);
+			if (callback) this.once("close", callback);
 			this.udp.close();
 		}
 
@@ -83,18 +96,45 @@ export module dtls {
 		private udp: dgram.Socket;
 
 		private udp_onListening() {
+			// connection successful
+			this._udpConnected = true;
+			if (this._connectionTimeout != null) clearTimeout(this._connectionTimeout);
 			// initialize record layer
 			this.recordLayer = new RecordLayer(this.udp, this.options);
+			// reuse the connection timeout for handshake timeout watching
+			this._connectionTimeout = setTimeout(this.expectHandshake, this.options.timeout);
 			// also start handshake
-			this.handshakeHandler = new ClientHandshakeHandler(this.recordLayer, this.options, () => {
-				// when done, emit "connected" event
-				this.emit("connected");
-				// also emit all buffered messages
-				while (this.bufferedMessages.length > 0) {
-					let {msg, rinfo} = this.bufferedMessages.shift();
-					this.emit("message", msg.data, rinfo);
+			this.handshakeHandler = new ClientHandshakeHandler(this.recordLayer, this.options, (err?: Error) => {
+				if (err) {
+					// something happened on the way to heaven
+					this.emit("error", err);
+					this.udp.close();
+				} else {
+					// when done, emit "connected" event
+					this._handshakeFinished = true;
+					if (this._connectionTimeout != null) clearTimeout(this._connectionTimeout);
+					this.emit("connected");
+					// also emit all buffered messages
+					while (this.bufferedMessages.length > 0) {
+						let {msg, rinfo} = this.bufferedMessages.shift();
+						this.emit("message", msg.data, rinfo);
+					}
 				}
 			});
+		}
+		// is called after the connection timeout expired. 
+		// Check the connection and throws if it is not established yet
+		private expectConnection() {
+			if (!this._udpConnected) {
+				// connection timed out
+				this.killConnection(new Error("The connection timed out"));
+			}
+		}
+		private expectHandshake() {
+			if (!this._handshakeFinished) {
+				// handshake timed out
+				this.killConnection(new Error("The DTLS handshake timed out"));
+			}
 		}
 
 		private udp_onMessage(udpMsg: Buffer, rinfo: dgram.RemoteInfo) {
@@ -116,7 +156,7 @@ export module dtls {
 						break;
 
 					case ContentType.application_data:
-						if (this.handshakeHandler.isHandshaking) {
+						if (!this._handshakeFinished) {
 							// if we are still shaking hands, buffer the message until we're done
 							this.bufferedMessages.push({msg, rinfo});
 						} else /* finished */ {
@@ -133,20 +173,37 @@ export module dtls {
 		private _isClosed: boolean = false;
 		private udp_onClose() {
 			this._isClosed = true;
+			// we no longer want to receive events
+			this.udp.removeAllListeners();
 			this.emit("close");
 		}
 		private udp_onError(exception: Error) {
 			this.emit("error", exception);
 		}
+
+		/** Kills the underlying UDP connection and emits an error if neccessary */
+		private killConnection(err?: Error) {
+			this._isClosed = true;
+			this.udp.removeAllListeners();
+			this.udp.close();
+			if (err != null) this.emit("error", err);
+		}
 		
 	}
 
 	export interface Options {
+		/** the type of the underlying socket */
 		type: "udp4" | "udp6";
+		/** ?? see NodeJS docs */
 		reuseAddr?: boolean;
+		/** The remote address to connect to */
 		address: string;
+		/** The remote port to connect to */
 		port: number;
+		/** Pre shared key information as a table <identity> => <psk> */
 		psk: { [identity: string]: string };
+		/** Time after which a connection should successfully established */
+		timeout?: number;
 		//keyContext?: any; // TODO: DTLS-security options
 	}
 
